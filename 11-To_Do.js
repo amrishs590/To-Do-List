2cb7987f3951c8068495da41e74cef4c27b98977
Amrish <152243725+amrishs590@users.noreply.github.com>
2025-04-12 09:45:54 +0530

    Update 11-To_Do.js

--- 11-To_Do.js
+++ 11-To_Do.js
@@ -1,14 +1,10 @@
 const todoList = [];
-
 renderTodoList();
-
 function renderTodoList() {
   let todoListHTML = "";
 
   for (let i = 0; i < todoList.length; i++) {
     const todoObject = todoList[i];
-    //const name = todoObject.name;
-    //const dueDate = todoObject.dueDate;
     const { name, dueDate } = todoObject;
     const html = `
         <div>${name}</div>
@@ -20,7 +16,6 @@ function renderTodoList() {
       `;
     todoListHTML += html;
   }
-
   document.querySelector(".js-todo-list").innerHTML = todoListHTML;
 }
 
@@ -32,13 +27,10 @@ function addTodo() {
   const dueDate = dateInputElement.value;
 
   todoList.push({
-    //name: name,
-    //dueDate: dueDate,
     name,
     dueDate,
   });
 
   inputElement.value = "";
-
   renderTodoList();
 }
